b7dbec9e7211d038e53266c6e0cfb80bd80f5a6f
Acanthaster <a.remazeilles@rubika-edu.com>
2020-04-16 19:34:09 +0200

    La version 2.0 du projet, sans problème de merge. Elle contient : le projet de départ, le ghost jump, le buffered jump, le wall jump et le switch de monde.

---
+++ Assets/AXD_Scripts/AXD_PlayerStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AXD_PlayerStatus : MonoBehaviour
+{
+    [Header("World")]
+    public bool LivingWorld;
+
+    [Header("Health")]
+    public int HealthPoint;
+
+    [Header("Collectibles")]
+    public int Corn;
+    public int Cacao;
+
+    public ALR_PlayerController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LivingWorld = true;
+        HealthPoint = 5;
+        Corn = 0;
+        Cacao = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
